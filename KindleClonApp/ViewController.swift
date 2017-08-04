bcfea7886663bb32fe389cfa37cbaf017701116e
viktor hugo <victorHbenitez85@hotmail.com>
2017-08-04 15:53:59 -0500

    if statements and For loops

--- KindleClonApp/ViewController.swift
+++ KindleClonApp/ViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-
 class Book {
     let title: String
     let author: String
@@ -44,15 +43,27 @@ class ViewController: UIViewController {
         let page2 = Page(number: 2, text: "This is the text for second page")
         
         let pages : [Page] = [page1, page2]
-//        print(page1.text)
-//        print(page2.text)
         
         let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
-//        print("Author \(book.author) is of the \(book.title) book")
         
-        // Access to the first page to the Steve Jobs Book
-        let firstPage = book.pages[0]
-        print(firstPage.text)
+
+        // Create new Book object
+        let book2 = Book(title: "Bill Gates: A Biography", author: "Michael Becraft",
+                         pages: [Page(number: 1, text: "Text for page 1"),
+                                 Page(number: 2, text: "Text for page 2"),
+                                 Page(number: 3, text: "Text for page 3"),
+                                 Page(number: 4, text: "Text for page 4")])
+        
+        // Loop of the books and the pages in it
+        for book in [book, book2]{
+            print(book.title)
+            for page in book.pages {
+                print(page.text)
+            }
+        
+        }
+        
+        
     }
 }
 
