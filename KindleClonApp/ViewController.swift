39a8ec7c44a8928aa19d82a5a2af426ebf22c01a
viktor hugo <victorHbenitez85@hotmail.com>
2017-08-04 15:20:11 -0500

    Create the model data for the app and relating them each other's

--- KindleClonApp/ViewController.swift
+++ KindleClonApp/ViewController.swift
@@ -8,18 +8,51 @@
 
 import UIKit
 
+
+class Book {
+    let title: String
+    let author: String
+    let pages: [Page]  // one to many : for that book class has the Page's object
+    
+    init(title: String, author: String, pages: [Page]) {
+        self.title = title
+        self.author = author
+        self.pages = pages
+    }
+    
+}
+
+class Page {
+    let number: Int
+    let text: String
+    
+    init(number: Int, text: String) {
+        self.number = number
+        self.text = text
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .red
+        // can provide custom code starting here
+        
+        let page1 = Page(number: 1, text: "Text for the first page")
+        let page2 = Page(number: 2, text: "This is the text for second page")
+        
+        let pages : [Page] = [page1, page2]
+//        print(page1.text)
+//        print(page2.text)
+        
+        let book = Book(title: "Steve Jobs", author: "Walter Isaacson", pages: pages)
+//        print("Author \(book.author) is of the \(book.title) book")
+        
+        // Access to the first page to the Steve Jobs Book
+        let firstPage = book.pages[0]
+        print(firstPage.text)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
