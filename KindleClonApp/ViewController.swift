870fd311cfd78de8565d01ff0a03bd1a5f6bb750
viktor hugo <victorHbenitez85@hotmail.com>
2017-08-06 04:35:58 -0500

    UITableView Cell Register and Dequeue

--- KindleClonApp/ViewController.swift
+++ KindleClonApp/ViewController.swift
@@ -15,12 +15,36 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellId")
+        tableView.tableFooterView = UIView()
+        
         navigationItem.title = "Kindle App"
         
-        view.backgroundColor = .red
         // can provide custom code starting here
         setupBooks()
         
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath)
+        
+        let book = books?[indexPath.row]
+        
+        cell.textLabel?.text = book?.title
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if let count = books?.count{
+            return count
+        }
+        return 0
     }
     
     func setupBooks(){
